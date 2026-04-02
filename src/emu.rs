609e50564be641c9a2577a4ab5c4fa1b398c8891
Daniel <danieljagodic@freenet.de>
2026-04-02 14:56:38 +0200

    Improved|Almost finished RW from Memory Bus

--- src/emu.rs
+++ src/emu.rs
@@ -1,8 +1,11 @@
 use crate::cartridge::{self, Cartridge};
 use crate::register::Register;
 
-const RAM_SIZE: usize = 1024 * 8; //8KiB
+const WRAM_SIZE: usize = 1024 * 8; //8KiB
 const VRAM_SIZE: usize = 1024 * 8; //8KiB
+const OAM_SIZE: usize = 0xA0;  // FE00-FE9F (160 bytes)
+const IO_SIZE: usize = 0x80;   // FF00-FF7F (128 bytes)
+const HRAM_SIZE: usize = 0x7F; // FF80-FFFE (127 bytes)
 
 pub struct Emulator {
     cartridge: Cartridge,
@@ -12,8 +15,13 @@ pub struct Emulator {
     pc: u16,
     sp: u16,
     //RAM
-    wram: [u8; RAM_SIZE],
+    wram: [u8; WRAM_SIZE],
     vram: [u8; VRAM_SIZE],
+    hram: [u8; HRAM_SIZE],
+    oam: [u8; OAM_SIZE],
+
+    io: [u8; IO_SIZE],
+
     //Display
     display: [[u8; 160]; 144],
 }
@@ -23,7 +31,7 @@ impl Emulator {
         &self.display
     }
 
-    //TODO: proper reading | ROM Banking
+    //TODO: proper ROM Banking, Interrupt
     pub fn read_byte(&self, addr: u16) -> u8 {
         match addr {
             //16KiB ROM Bank
@@ -33,15 +41,22 @@ impl Emulator {
             //VRAM
             0x8000..=0x9FFF => self.vram[addr as usize - 0x8000],
             //8KiB External RAM?
-            0xA000..=0xBFFF => 0,
+            0xA000..=0xBFFF => {
+                let ram_addr = addr as usize - 0xA000;
+                if ram_addr < self.cartridge.ram.len() {
+                    self.cartridge.ram[ram_addr]
+                } else {
+                    0xFF
+                }
+            },
             //8KiB WRAM
             0xC000..=0xDFFF => self.wram[addr as usize - 0xC000],
             //OAM
-            0xFE00..=0xFE9F => 0,
+            0xFE00..=0xFE9F => self.oam[addr as usize - 0xFE00],
             //IO Registers
-            0xFF00..=0xFF7F => 0,
+            0xFF00..=0xFF7F => self.io[addr as usize - 0xFF00],
             //HRAM
-            0xFF80..=0xFFFE => 0,
+            0xFF80..=0xFFFE => self.hram[addr as usize - 0xFF80],
             //Interrupt
             0xFFFF => 0,
             //Echo RAM 0xE000..=0xFDFF and 0xFEA0..=0xFEFF prohibited
@@ -49,7 +64,7 @@ impl Emulator {
         }
     }
 
-    //TODO: proper writing
+    //TODO: proper writing, Interrupt
     pub fn write(&mut self, addr: u16, value: u8) {
         match addr {
             //==ROM Bank== IGNORE
@@ -57,18 +72,23 @@ impl Emulator {
             //VRAM
             0x8000..=0x9FFF => self.vram[addr as usize - 0x8000] = value,
             //8KiB External RAM?
-            0xA000..=0xBFFF => (),
+            0xA000..=0xBFFF => {
+                let ram_addr = addr as usize - 0xA000;
+                if ram_addr < self.cartridge.ram.len() {
+                    self.cartridge.ram[ram_addr] = value;
+                }
+            },
             //8KiB WRAM
             0xC000..=0xDFFF => self.wram[addr as usize - 0xC000] = value,
             //OAM
-            0xFE00..=0xFE9F => (),
+            0xFE00..=0xFE9F => self.oam[addr as usize - 0xFE00] = value,
             //IO Registers
-            0xFF00..=0xFF7F => (),
+            0xFF00..=0xFF7F => self.io[addr as usize - 0xFF00] = value,
             //HRAM
-            0xFF80..=0xFFFE => (),
+            0xFF80..=0xFFFE => self.hram[addr as usize - 0xFF80] = value,
             //Interrupt
             0xFFFF => (),
-            //Echo RAM 0xE000..=0xFDFF and 0xFEA0..=0xFEFF prohibited
+            //Echo RAM 0xE000..=0xFDFF and 0xFEA0..=0xFEFF prohibited ?> IGNORE
             _ => (),
         }
     }
@@ -78,8 +98,12 @@ impl Emulator {
             pc: 0x0100,
             sp: 0xFFFE,
             registers: Register::new(),
-            wram: [0; RAM_SIZE],
-            vram: [0; RAM_SIZE],
+            wram: [0; WRAM_SIZE],
+            vram: [0; WRAM_SIZE],
+            hram: [0; HRAM_SIZE],
+            oam: [0; OAM_SIZE],
+            io: [0; IO_SIZE],
+
             display: [[0; 160]; 144],
 
             cartridge: Cartridge::new(rom_path),
